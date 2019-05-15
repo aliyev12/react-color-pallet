39ae2c16d04cd1b951bc80ca1105637b8068d880
aliyev12 <aaa719@nyu.edu>
2019-05-14 20:53:41 -0400

    Add select and material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import { Select, MenuItem } from "@material-ui/core";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import './Navbar.css';
+import "./Navbar.css";
 
-const Navbar = ({ level, setLevel }) => (
-  <header className="Navbar">
-    <div className="logo">
-      <a href="#">reactcolorpicker</a>
-    </div>
-    <div className="slider-container">
-      <span>Level: {level}</span>
-      <div className="slider">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={setLevel}
-        />
-      </div>
-    </div>
-  </header>
-);
+const Navbar = ({ level, setLevel, changeFormat }) => {
+    const [format, setFormat] = useState('hex');
+    const handleChange = async e => {
+        await setFormat(e.target.value);
+        changeFormat(e.target.value);
+    }
+
+    return (
+        <header className="Navbar">
+          <div className="logo">
+            <a href="#">reactcolorpicker</a>
+          </div>
+          <div className="slider-container">
+            <span>Level: {level}</span>
+            <div className="slider">
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={setLevel}
+              />
+            </div>
+          </div>
+          <div className="select-container">
+            <Select value={format} onChange={handleChange}>
+              <MenuItem value="hex">HEX - #ffffff</MenuItem>
+              <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+              <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+            </Select>
+          </div>
+        </header>
+      );
+}
+
+
 
 export default Navbar;
--- src/Palette.js
+++ src/Palette.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import ColorBox from "./ColorBox";
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 import "./Palette.css";
 
 const Palette = ({ palette: { colors } }) => {
   const [level, setLevel] = useState(500);
+  const [format, changeFormat] = useState('hex');
 
   const colorBoxes = colors[level].map(color => (
-    <ColorBox background={color.hex} name={color.name} />
+    <ColorBox background={color[format]} name={color.name} />
   ));
   return (
     <div className="Palette">
-    <Navbar level={level} setLevel={newLevel => setLevel(newLevel)} />
+      <Navbar
+        level={level}
+        setLevel={newLevel => setLevel(newLevel)}
+        changeFormat={value => changeFormat(value)}
+      />
       {/* Navbar goes here */}
       <div className="Palette-colors">
         {/* bunch of colors */}
