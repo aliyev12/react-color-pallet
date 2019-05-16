2e1d9077fcb879eb204078becd60333fea6cc28e
aliyev12 <aaa719@nyu.edu>
2019-05-15 21:04:23 -0400

    Fix route palette

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
-import { generatePalette } from './colorHelpers';
+import React from "react";
+import Palette from "./Palette";
+import { Route, Switch } from "react-router-dom";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 const App = props => {
-  console.log(generatePalette(seedColors[4]));
+  const findPalette = id => {
+    return seedColors.find(palette => {
+      return palette.id === id;
+    });
+  };
   return (
-    <div className="">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>palette here</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={routeProps => (
+          <Palette
+            palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
+
+    // <div className="">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
-}
+};
 
 export default App;
